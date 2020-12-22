fadd8213425685c5bb73f547ce5cae04c9dec01b
4rth4s <51023316+baobaovt@users.noreply.github.com>
2020-12-22 11:51:34 +0700

    Add files via upload

--- ProjectOOP/src/quanlybenhnhan/Info.java
+++ ProjectOOP/src/quanlybenhnhan/Info.java
@@ -7,7 +7,7 @@ public class Info {
 	Checkin_Day ci = new Checkin_Day();
 	Checkout_Day co = new Checkout_Day();
 	BirthDay b = new BirthDay();
-	private String Id;
+	private int Id;
 	private String Name;
 	private String Gender;
 	private Id_Address Address;
@@ -20,23 +20,21 @@ public class Info {
 	}
 		
 
-	public Info(String Name,String Phone, String Id, Id_Address Address, String Gender) {
+	public Info(int Id,String Name, String Gender) {
 		this.Name = Name;
-		this.Phone = Phone;
 		this.Id = Id;
-		this.Address = Address;
 		this.Gender = Gender;
 	}
 	
 	
 
 
-	public String getId() {
+	public  int getId() {
 		return Id;
 	}
 
 
-	public void setId(String id) {
+	public void setId(int id) {
 		Id = id;
 	}
 
@@ -72,9 +70,8 @@ public class Info {
 
 
 	public void insertInfo() {
-		
-		System.out.println("NHAP MSBN " );
-		this.Id = sc.next();
+		getId();
+		System.out.println(" MSBN " +Id );
 		System.out.println("NHAP HO TEN BENH NHAN ");
 		Name = sc.next();
 		sc.nextLine();
@@ -88,12 +85,6 @@ public class Info {
 		
 	}
 
-	@Override
-	public String toString() {
-		return "THONG TIN BENH NHAN [ID=" + Id + ", Name=" + Name + ", Gender=" + Gender + ", Address=" + Address
-				+ ", Phone=" + Phone + "]";
-		
-	}
 	public void display_CV() {
 		System.out.println("THONG TIN BENH NHAN [ID=" + Id + ", Name=" + Name + ", Gender=" + Gender + ", Address=" + Address+ ", Phone=" + Phone + ")");
 	}
